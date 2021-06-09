8993d79f515e42ae1b7239a5be908fe3d2814f94
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-06-09 13:56:53 -0700

    varying batch sizesg

--- computing_p.py
+++ computing_p.py
@@ -8,13 +8,14 @@ import math
 import numpy as np
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+from util.data import get_data, normalize_data
 
-batch_size = 10
+# batch_size = 10
 shuffle=True
 num_workers = 1
 # depth = 50
 
-seed = 5
+seed = 1
 torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -73,36 +74,39 @@ def main():
                        ]))
 
 
-    training_loader = DataLoader(trainset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size,drop_last=False)
-    test_loader = DataLoader(testset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size,drop_last=False)
+    lowest_c1_alpha_val = []
+    batch_sizes = [2, 5, 10, 20, 50, 100, 200]
+    for batch_size in tqdm(batch_sizes):
+        training_loader = DataLoader(trainset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size,drop_last=False)
+        test_loader = DataLoader(testset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size,drop_last=False)
 
 
-    train_data_x, train_data_y = next(iter(training_loader))
-    test_data_x, test_data_y = next(iter(test_loader))
-    train_data = train_data_x
-    train_data_label = train_data_y
-    test_data = test_data_x
-    test_data_label = test_data_y
+        train_data_x, train_data_y = next(iter(training_loader))
+        test_data_x, test_data_y = next(iter(test_loader))
+        train_data = train_data_x
+        train_data_label = train_data_y
+        test_data = test_data_x
+        test_data_label = test_data_y
 
-    data = train_data
-    labels = train_data_label
+        data = train_data
+        labels = train_data_label
 
 
-    data = torch.flatten(data, start_dim = 2, end_dim = 3).squeeze()
-    norms = torch.norm(data, dim=1)
-    normed_data_final = torch.div(data.T, norms).T
-    labels = labels.type(torch.FloatTensor)
+        data = torch.flatten(data, start_dim = 2, end_dim = 3).squeeze()
+        norms = torch.norm(data, dim=1)
+        normed_data_final = torch.div(data.T, norms).T
+        labels = labels.type(torch.FloatTensor)
 
-    model = PBoundNetwork()
+        model = PBoundNetwork()
 
     # torch.autograd.set_detect_anomaly(True)
 
 
 
 
-    lowest_c1_alpha_val = []
-    depth_vals = [2, 3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100]
-    for depth in tqdm(depth_vals):
+    # lowest_c1_alpha_val = []
+    # depth_vals = [2, 3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100]
+    # for depth in tqdm(depth_vals):
         # results_arr = []
         outputs_arr = []
         alpha_vals = np.linspace(0, 10, 1000)
@@ -110,7 +114,7 @@ def main():
             alpha = Variable(torch.Tensor([alpha_val]), requires_grad=True)
             if alpha.grad is not None:
                 alpha.grad.data.zero_()
-            outputs = model(normed_data_final, labels, alpha, depth)
+            outputs = model(normed_data_final, labels, alpha, 50)
 
             outputs.backward()
 
@@ -138,11 +142,11 @@ def main():
         lowest_c1_alpha_val.append(alpha_val_index_min)
 
     lowest_c1_alpha_val = np.array(lowest_c1_alpha_val)
-    plt.plot(depth_vals, lowest_c1_alpha_val)
-    plt.title("Depth vs optimal alpha value (fixed dataset size batch size 10 MNIST)")
+    plt.plot(batch_sizes, lowest_c1_alpha_val)
+    plt.title("Batch size vs optimal alpha value (fixed depth = 50) MNIST)")
     plt.show()
 
-    np.save(open("lowest_c1_alpha_val.npy", "wb"), lowest_c1_alpha_val)
+    np.save(open("lowest_c1_alpha_val_batch_sizes.npy", "wb"), lowest_c1_alpha_val)
 
 if __name__ == "__main__":
     main()
