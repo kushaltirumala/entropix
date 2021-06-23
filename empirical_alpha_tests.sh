b93136fdbd3553f7ef3346956a5bd4fba418c328
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-06-23 00:09:24 -0700

    retrying

--- empirical_alpha_tests.sh
+++ empirical_alpha_tests.sh
@@ -1,5 +1,11 @@
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=40
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=60
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=100
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=32
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=7
+# python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=40
+# python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=60
+# python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=100
+# python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=32
+# python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=7
+
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=40
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=60
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=100
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=32
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=7
