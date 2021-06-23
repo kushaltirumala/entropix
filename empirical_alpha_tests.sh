0d5f5e074f9375959295f5661f9b652b046a33da
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-06-23 12:59:58 -0700

    making only width higher

--- empirical_alpha_tests.sh
+++ empirical_alpha_tests.sh
@@ -1,8 +1,8 @@
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=41
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=61
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=101
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=31
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=71
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=1000 --width=5000 --seed=41
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=1000 --width=5000 --seed=61
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=1000 --width=5000 --seed=101
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=1000 --width=5000 --seed=31
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=1000 --width=5000 --seed=71
 
 # python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=40
 # python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5 --width=5 --seed=60
