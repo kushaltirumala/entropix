f8c55c51ebad1136d466d0ff7c9560b8794a4b53
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-06-25 15:07:35 -0700

    doing 10 more seeds

--- empirical_alpha_tests.sh
+++ empirical_alpha_tests.sh
@@ -1,11 +1,11 @@
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=45
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=65
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=105
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=85
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=45
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=95
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=205
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=305
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=48
-python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=97
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=47
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=63
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=101
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=83
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=43
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=93
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=203
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=303
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=53
+python3 empirical_alpha_tests.py --batch_size=10 --num_networks=5000 --width=1000 --seed=113
 
